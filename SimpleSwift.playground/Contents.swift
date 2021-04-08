fd6a2c02dc52cdeb2c1ef9b0abab99184e55d040
Megan <calvem2@cs.washington.edu>
2021-04-07 18:25:53 -0700

    implement positive and negative number calculations

--- SimpleSwift.playground/Contents.swift
+++ SimpleSwift.playground/Contents.swift
@@ -1,11 +1,66 @@
+import Foundation
+
 print("Welcome to the UW Calculator Playground")
 
 func calculate(_ args: [String]) -> Int {
-    return -1
+    // handle count, avg, factorial
+    switch args[args.count - 1] {
+    case "count":
+        return args.count - 1
+    case "avg":
+        var total = 0
+        let count = args.count - 1
+        for n in 0...count {
+            total += Int(args[n]) ?? 0
+        }
+        return count != 0 ? total / count : 0
+    case "fact":
+        let num = Int(args[0]) ?? -1
+        if num == -1 {
+            return 0
+        } else if num == 0 {
+            return 1
+        }
+        var fact = 1;
+        for n in 1...num {
+            fact *= n
+        }
+        return fact
+    default:
+        break
+    }
+    
+    // handle expressions
+    var operation = "+"
+    var value = 0
+    for arg in args {
+        if arg == "+" || arg == "-" || arg == "*" || arg == "/" || arg == "%" {
+            operation = arg
+        } else {
+            switch operation {
+            case "+":
+                value += Int(arg) ?? 0
+            case "-":
+                value -= Int(arg) ?? 0
+            case "*":
+                value *= Int(arg) ?? 1
+            case "/":
+                value /= Int(arg) ?? 1
+            case "%":
+                let num = Int(arg) ?? nil
+                if num != nil {
+                    value %= num!
+                }
+            default:
+                break
+            }
+        }
+    }
+    return value;
 }
 
 func calculate(_ arg: String) -> Int {
-    return -1
+    return calculate(arg.components(separatedBy: " "))
 }
 
 // -------------------------------------------
@@ -53,7 +108,7 @@ calculate("5 fact") == 120
 
 // Implement calculate([String]) and calculate(String)
 // to handle negative numbers
-/*
+
 calculate(["2", "+", "-2"]) == 0
 calculate(["2", "-", "-2"]) == 4
 calculate(["2", "*", "-2"]) == -4
@@ -68,7 +123,7 @@ calculate("2 - -2") == 4
 calculate("-2 / 2") == -1
 
 calculate("1 -2 3 -4 5 count") == 5
-*/
+
  
 // Implement calculate([String]) and calculate(String)
 // to use floating-point values
